1ca10e4406f4d090471679017723cf7c3d57364c
Luiz Antonio de Souza <luiz.antoniodesouza004@gmail.com>
2024-10-09 13:43:50 -0300

    reactNative

---
+++ components/Teste.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export function Teste() {
+    return(
+        <View>
+            <Text>
+                Sujeito Programador
+            </Text>
+        </View>
+        // Em React TNative não se pode colocar texto solto
+    )
+}
